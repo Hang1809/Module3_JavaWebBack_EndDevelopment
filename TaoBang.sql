ce065e185cb6b2cfd32da304e049526d0bd38fc9
Hang1809 <hangdinhphankhanh@gmail.com>
2022-07-01 05:58:55 +0700

    TaoBang

---
+++ TaoBang.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE QuanLyDiemThi;
+USE QuanLyDiemThi;
+CREATE TABLE HocSinh(
+MaHS VARCHAR(20) PRIMARY KEY,
+TenHS VARCHAR(50),
+NgaySinh DATETIME,
+Lop VARCHAR(20),
+GT VARCHAR(20)
+);
+CREATE TABLE MonHoc(
+MaHS VARCHAR(20) PRIMARY KEY,
+TenMH VARCHAR(50),
+MaMH VARCHAR(20));
+
+CREATE TABLE BangDiem(
+MaHS VARCHAR(20),
+MaMH VARCHAR(20),
+DiemThi INT,
+NgayKT datetime,
+primary key (MaHS, MaMH),
+foreign key (MaHS) references HocSinh(MaHS));
+-- foreign key (MaMH) references MonHoc (TenMH));
+
+CREATE TABLE GiaoVien(
+    MaGV VARCHAR(20) PRIMARY KEY,
+    TenGV VARCHAR(20),
+    SDT VARCHAR(10)
+);
+
+ALTER TABLE MonHoc ADD MaGV VARCHAR(20);
+ALTER TABLE MonHoc ADD CONSTRAINT FK_MaGV FOREIGN KEY (MaGV) REFERENCES GiaoVien(MaGV);
